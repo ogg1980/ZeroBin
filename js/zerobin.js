24956eff15dfa60fec22bba5016b6b016ba14b00
Simone <chaufnet@gmail.com>
2014-12-29 23:50:23 +0100

    Fix highlightjs

--- js/zerobin.js
+++ js/zerobin.js
@@ -194,10 +194,6 @@ function setElementText(element, text, isDataUrl) {
 /** Apply syntax coloring to clear text area.
  */
 function applySyntaxColoring() {
-    if ($('div#cleartext').html().substring(0, 11) != '<pre><code>') {
-        // highlight.js expects code to be surrounded by <pre><code>
-        $('div#cleartext').html('<pre><code>' + $('div#cleartext').html() + '</code></pre>');
-    }
     hljs.highlightBlock(document.getElementById('cleartext'));
     $('div#cleartext').css('padding', '0'); // Remove white padding around code box.
 }
